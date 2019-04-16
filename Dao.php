1595716916faddb920e1c6213e33e0513afbacb6
CodyPalin <43323493+CodyPalin@users.noreply.github.com>
2019-04-15 18:25:53 -0600

    Update Dao.php

--- Dao.php
+++ Dao.php
@@ -3,7 +3,7 @@ class Dao {
 	private $host ="us-cdbr-iron-east-03.cleardb.net";
 	private $db = "heroku_37bb6c10c4cefc0";
 	private $user = "b8df8be23a0e15";
-	private $pass = "d9cf9f72";
+	//private $pass = [hidden for security reasons];
 	public function getConnection() {
 		return new PDO("mysql:host={$this->host};dbname={$this->db}",$this->user,$this->pass);
 	}
